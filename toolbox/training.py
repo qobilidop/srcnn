4ffbd0860128694aa8ac82e75cd4000cbebcab4b
Bili Dong <qobilidop@gmail.com>
2017-03-12 15:04:02 -0700

    Refactor training process
    Save model architecture and model weights separately. Save model weights at each epoch.

--- toolbox/training.py
+++ toolbox/training.py
@@ -1,6 +1,6 @@
 import os
 
-from keras.models import load_model
+from keras.models import model_from_yaml
 from keras.callbacks import CSVLogger
 from keras.callbacks import ModelCheckpoint
 import pandas as pd
@@ -8,26 +8,39 @@ import pandas as pd
 
 def train(model, x, y, batch_size=32, nb_epoch=1, validation_data=None,
           optimizer='adam', loss='mse', metrics=[],
-          resume=False, save_dir='save'):
+          save_dir='save', resume=True):
+    # Ensure save directory exists
     os.makedirs(save_dir, exist_ok=True)
-    model_file = os.path.join(save_dir, 'model.hdf5')
-    history_file = os.path.join(save_dir, 'history.csv')
 
-    if resume and os.path.exists(model_file):
-        model = load_model(model_file)
+    # Check architecture
+    config_file = os.path.join(save_dir, 'config.yaml')
+    if resume and os.path.exists(config_file):
+        # Check architecture consistency
+        saved_model = model_from_yaml(open(config_file).read())
+        if model.get_config() != saved_model.get_config():
+            raise ValueError('Model architecture has changed.')
     else:
-        model.compile(optimizer, loss, metrics=metrics)
+        # Save architecture
+        open(config_file, 'w').write(model.to_yaml())
 
-    # set up callbacks to record model and history
-    model_checkpoint = ModelCheckpoint(model_file)
+    # Set up loggers
+    weights_file_template = os.path.join(save_dir, 'weights.{epoch:04d}.hdf5')
+    model_checkpoint = ModelCheckpoint(weights_file_template,
+                                       save_best_only=True)
+    history_file = os.path.join(save_dir, 'history.csv')
     csv_logger = CSVLogger(history_file, append=resume)
 
-    # determine initial epoch
+    # Inherit weights
     if resume and os.path.exists(history_file):
         initial_epoch = pd.read_csv('save/history.csv')['epoch'].iloc[-1] + 1
     else:
         initial_epoch = 0
+    weights_file = weights_file_template.format(epoch=initial_epoch - 1)
+    if os.path.exists(weights_file):
+        model.load_weights(weights_file)
 
+    # Train
+    model.compile(optimizer, loss, metrics=metrics)
     model.fit(x, y, batch_size=batch_size, nb_epoch=nb_epoch,
               callbacks=[model_checkpoint, csv_logger],
               validation_data=validation_data,
