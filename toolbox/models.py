79e1b5b705e5107741e8a17a8aa4023675a56809
Bili Dong <qobilidop@gmail.com>
2017-03-12 17:35:22 -0700

    Add ReLU activations

--- toolbox/models.py
+++ toolbox/models.py
@@ -4,8 +4,8 @@ from keras.models import Sequential
 
 def srcnn(input_shape, c=1, f1=9, f2=1, f3=5, n1=64, n2=32):
     model = Sequential()
-    model.add(Conv2D(nb_filter=n1, nb_row=f1, nb_col=f1,
+    model.add(Conv2D(nb_filter=n1, nb_row=f1, nb_col=f1, activation='relu',
                      input_shape=input_shape))
-    model.add(Conv2D(nb_filter=n2, nb_row=f2, nb_col=f2))
+    model.add(Conv2D(nb_filter=n2, nb_row=f2, nb_col=f2, activation='relu'))
     model.add(Conv2D(nb_filter=c, nb_row=f3, nb_col=f3))
     return model
