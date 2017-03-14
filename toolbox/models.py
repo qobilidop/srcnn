5c8e83373a854242aca7a82611d47d1fdb85269e
Bili Dong <qobilidop@gmail.com>
2017-03-14 10:29:58 -0700

    Use he_normal initialization for all layers

--- toolbox/models.py
+++ toolbox/models.py
@@ -4,10 +4,9 @@ from keras.models import Sequential
 
 def srcnn(input_shape, c=1, f1=9, f2=1, f3=5, n1=64, n2=32):
     model = Sequential()
-    model.add(Conv2D(nb_filter=n1, nb_row=f1, nb_col=f1,
-                     activation='relu', init='he_normal',
-                     input_shape=input_shape))
-    model.add(Conv2D(nb_filter=n2, nb_row=f2, nb_col=f2,
-                     activation='relu', init='he_normal'))
-    model.add(Conv2D(nb_filter=c, nb_row=f3, nb_col=f3))
+    model.add(Conv2D(nb_filter=n1, nb_row=f1, nb_col=f1, init='he_normal',
+                     activation='relu', input_shape=input_shape))
+    model.add(Conv2D(nb_filter=n2, nb_row=f2, nb_col=f2, init='he_normal',
+                     activation='relu'))
+    model.add(Conv2D(nb_filter=c, nb_row=f3, nb_col=f3, init='he_normal'))
     return model
