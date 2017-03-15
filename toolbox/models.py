de84be15c4c519a680121e15c26d6eed6d091cd2
Bili Dong <qobilidop@gmail.com>
2017-03-15 11:12:44 -0700

    Upgrade to Keras 2 API

--- toolbox/models.py
+++ toolbox/models.py
@@ -10,10 +10,10 @@ def compile_srcnn(input_shape, c=1, f1=9, f2=1, f3=5, n1=64, n2=32):
     See https://arxiv.org/abs/1501.00092.
     """
     model = Sequential()
-    model.add(Conv2D(nb_filter=n1, nb_row=f1, nb_col=f1, init='he_normal',
+    model.add(Conv2D(n1, f1, kernel_initializer='he_normal',
                      activation='relu', input_shape=input_shape))
-    model.add(Conv2D(nb_filter=n2, nb_row=f2, nb_col=f2, init='he_normal',
+    model.add(Conv2D(n2, f2, kernel_initializer='he_normal',
                      activation='relu'))
-    model.add(Conv2D(nb_filter=c, nb_row=f3, nb_col=f3, init='he_normal'))
+    model.add(Conv2D(c, f3, kernel_initializer='he_normal'))
     model.compile(optimizer='adam', loss='mse', metrics=[psnr])
     return model
