238c35d515327135171680cd5c0a2176dedef557
Bili Dong <qobilidop@gmail.com>
2017-03-15 19:08:30 -0700

    Refactor data module
    Now we assume the CNN uses 'same' padding, so the image size does not reduce through the convolutional layers. So the HR image size is strictly LR image size multiplied by the scale factor. This assumption makes the code much cleaner.
    
    Also the saving data to hdf5 part is removed. We are not actually using this functionality. And `load_set` does not take a long time to run.

--- toolbox/data.py
+++ toolbox/data.py
@@ -1,63 +1,45 @@
-import h5py
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
-from keras.preprocessing.image import list_pictures
 
 from toolbox.paths import data_dir
 from toolbox.preprocessing import bicubic_resize
 from toolbox.preprocessing import modcrop
 
 
-def load_data(train_dir='Train', test_dir='Test/Set5', channel=0):
-    x_train, y_train = generate(train_dir, channel=channel)
-    x_test, y_test = generate(test_dir, save_path='test.h5', stride=21,
-                              channel=channel)
-    return (x_train, y_train), (x_test, y_test)
+def load_set(name, sub_size=7, sub_stride=3, scale=3, channel=0):
+    dataset_dir = data_dir / name
+    lr_sub_arrays = []
+    hr_sub_arrays = []
+    for path in dataset_dir.glob('*'):
+        lr_image, hr_image = load_image_pair(str(path), scale=scale)
+        lr_sub_arrays += [img_to_array(img) for img in generate_sub_images(
+            lr_image, sub_size, sub_stride)]
+        hr_sub_arrays += [img_to_array(img) for img in generate_sub_images(
+            hr_image, sub_size * scale, sub_stride * scale)]
+    x = np.stack(lr_sub_arrays)
+    y = np.stack(hr_sub_arrays)
+    x = restrict_channel(x, channel)
+    y = restrict_channel(y, channel)
+    return x, y
 
 
-def generate(directory, save_path='train.h5', size_input=33, size_label=21,
-             scale=3, stride=14, channel=0):
-    # settings
-    directory = data_dir / directory
+def load_image_pair(path, scale=3):
+    image = load_img(path)
+    image = image.convert('YCbCr')
+    hr_image = modcrop(image, scale)
+    lr_image = bicubic_resize(hr_image, 1 / scale)
+    return lr_image, hr_image
 
-    # initialization
-    data = []
-    label = []
-    padding = abs(size_input - size_label) // 2
 
-    # generate data
-    filepaths = list_pictures(str(directory), ext='bmp')
+def generate_sub_images(image, crop_size, stride):
+    for i in range(0, image.size[0] - crop_size + 1, stride):
+        for j in range(0, image.size[1] - crop_size + 1, stride):
+            yield image.crop([i, j, i + crop_size, j + crop_size])
 
-    for path in filepaths:
-        image = load_img(path)
-        image = image.convert('YCbCr')
 
-        im_label = modcrop(image, scale)
-        im_input = bicubic_resize(bicubic_resize(im_label, 1 / scale),
-                                  im_label.size)
-        im_label = img_to_array(im_label)
-        im_input = img_to_array(im_input)
-
-        hei, wid = im_label.shape[:2]
-        for x in range(0, hei - size_input + 1, stride):
-            for y in range(0, wid - size_input + 1, stride):
-                subim_input = im_input[x: x + size_input, y: y + size_input]
-                subim_label = im_label[x + padding: x + padding + size_label,
-                              y + padding: y + padding + size_label]
-                data += [subim_input]
-                label += [subim_label]
-
-    order = np.random.permutation(len(data))
-    data = np.stack(data)[order]
-    label = np.stack(label)[order]
-
-    with h5py.File(save_path, 'w') as f:
-        f.create_dataset('dat', data=data)
-        f.create_dataset('lab', data=label)
-
-    if channel == 'all':
-        return data, label
+def restrict_channel(array, channel):
+    if channel != 'all':
+        return array[:, :, :, channel:channel + 1]
     else:
-        return data[:, :, :, channel:channel + 1], \
-               label[:, :, :, channel:channel + 1]
+        return array
