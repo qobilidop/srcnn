cdccedb8c61481079eb6c7dc7a8a6fb4da0816c5
jiacong1990 <lijiacong1990@gmail.com>
2017-03-16 23:42:21 -0700

    Update run.py

--- experiments/srcnn-9-5-5_sc4/run.py
+++ experiments/srcnn-9-5-5_sc4/run.py
@@ -1,1 +1,29 @@
+from functools import partial
 
+from toolbox.data import load_set
+from toolbox.models import compile_srcnn
+from toolbox.experiment import Experiment
+from toolbox.preprocessing import bicubic_resize
+
+
+# Model
+scale = 4
+model = compile_srcnn(c=1, f1=9, f2=5, f3=5, n1=64, n2=32)
+model.summary()
+
+# Data
+train_set = '91-image'
+val_set = 'Set5'
+test_sets = ['Set5', 'Set14']
+preprocess = partial(bicubic_resize, size=scale)
+load_set = partial(load_set, sub_size=11, sub_stride=5, scale=scale,
+                   channel=0, preprocess=preprocess)
+
+# Training
+experiment = Experiment(scale=scale, model=model, preprocess=preprocess,
+                        load_set=load_set, save_dir='.')
+experiment.train(train_set=train_set, val_set=val_set, epochs=500, resume=True)
+
+# Evaluation
+for test_set in test_sets:
+    experiment.test(test_set=test_set)
