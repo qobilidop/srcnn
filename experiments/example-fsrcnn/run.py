c1c5704af206fc0b85091af816fdc33a4c941f3d
Bili Dong <qobilidop@gmail.com>
2017-03-18 11:16:06 -0700

    Test FSRCNN and add an example

---
+++ experiments/example-fsrcnn/run.py
@@ -0,0 +1,28 @@
+"""Example experiment."""
+from functools import partial
+
+from toolbox.data import load_set
+from toolbox.models import compile
+from toolbox.models import fsrcnn
+from toolbox.experiment import Experiment
+
+
+# Model
+scale = 3
+model = compile(fsrcnn(c=1, d=56, s=12, m=4, k=3))
+model.summary()
+
+# Data
+train_set = '91-image'
+val_set = 'Set5'
+test_sets = ['Set5', 'Set14']
+load_set = partial(load_set, sub_size=20, sub_stride=100, scale=scale)
+
+# Training
+experiment = Experiment(scale=scale, model=model,
+                        load_set=load_set, save_dir='.')
+experiment.train(train_set=train_set, val_set=val_set, epochs=2, resume=True)
+
+# Evaluation
+for test_set in test_sets:
+    experiment.test(test_set=test_set)
--- toolbox/models.py
+++ toolbox/models.py
@@ -25,42 +25,38 @@ def srcnn(c=1, f1=9, f2=1, f3=5, n1=64, n2=32):
     model.add(Conv2D(c, f3, padding='same', kernel_initializer='he_normal'))
     return model
 
-#def compile_espcn(c=1, f1=5, f2=3, f3=3, n1=64, n2=32, n3=9, f4=3, s1 = 3):
-def compile_espcn(c=1, f1=5, f2=3, f3=3, n1=64, n2=32, n3=1, f4=3, s1 = 1):
-    """Compile an ESPCN model.
-    
-    See https://arxiv.org/abs/1609.05158
-    """
-    model = Sequential()
-    model.add(Conv2D(n1, f1, padding='same', kernel_initializer='he_normal', activation='tanh', input_shape=(None, None, c)))
-    model.add(Conv2D(n2, f2, padding='same', kernel_initializer='he_normal', activation='tanh'))
-    model.add(Conv2D(c, f3, padding='same', kernel_initializer='he_normal'))
-    model.add(Conv2DTranspose(n3, f4, strides = s1, padding='same', kernel_initializer='he_normal'))
-    model.compile(optimizer='adam', loss='mse', metrics=[psnr])
-    return model
 
-
-def fsrcnn(c=1, f1=5, f2=1, f3=3, f4=9, n1=56, n2=12, n3=1, s1=3):
+def fsrcnn(c=1, d=56, s=12, m=4, k=3):
     """Build an FSRCNN model.
 
     See http://link.springer.com/chapter/10.1007/978-3-319-46475-6_25
     """
+    conv_params = dict(padding='same', kernel_initializer='he_normal',
+                       activation='relu')
     model = Sequential()
-    model.add(Conv2D(n1, f1, padding='valid', kernel_initializer='he_normal',
-                     input_shape=(None, None, c)))
-    model.add(PReLU())
-    model.add(Conv2D(n2, f2, kernel_initializer='he_normal'))
-    model.add(PReLU())
-    model.add(Conv2D(n2, f3, padding='same', kernel_initializer='he_normal'))
-    model.add(PReLU())
-    model.add(Conv2D(n2, f3, padding='same', kernel_initializer='he_normal'))
-    model.add(PReLU())
-    model.add(Conv2D(n2, f3, padding='same', kernel_initializer='he_normal'))
-    model.add(PReLU())
-    model.add(Conv2D(n2, f3, padding='same', kernel_initializer='he_normal'))
-    model.add(PReLU())
-    model.add(Conv2D(n1, f2, kernel_initializer='he_normal'))
-    model.add(PReLU())
+    model.add(Conv2D(d, 5, input_shape=(None, None, c), **conv_params))
+    model.add(Conv2D(s, 1, **conv_params))
+    for i in range(m):
+        model.add(Conv2D(3, s, **conv_params))
+    model.add(Conv2D(d, 1, **conv_params))
+    model.add(Conv2DTranspose(1, 9, strides=k, padding='same',
+                              kernel_initializer='he_normal'))
+    return model
+
+
+# def compile_espcn(c=1, f1=5, f2=3, f3=3, n1=64, n2=32, n3=9, f4=3, s1 = 3):
+def compile_espcn(c=1, f1=5, f2=3, f3=3, n1=64, n2=32, n3=1, f4=3, s1=1):
+    """Compile an ESPCN model.
+
+    See https://arxiv.org/abs/1609.05158
+    """
+    model = Sequential()
+    model.add(Conv2D(n1, f1, padding='same', kernel_initializer='he_normal',
+                     activation='tanh', input_shape=(None, None, c)))
+    model.add(Conv2D(n2, f2, padding='same', kernel_initializer='he_normal',
+                     activation='tanh'))
+    model.add(Conv2D(c, f3, padding='same', kernel_initializer='he_normal'))
     model.add(Conv2DTranspose(n3, f4, strides=s1, padding='same',
                               kernel_initializer='he_normal'))
+    model.compile(optimizer='adam', loss='mse', metrics=[psnr])
     return model
