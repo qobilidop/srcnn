2bba8c780196d62b4e7e2f0f4a5e6c72b120c698
Bili Dong <qobilidop@gmail.com>
2017-03-19 11:48:06 -0700

    PReLU still not working
    We tried to use PReLU but haven't figured out a solution. The issue is that the trainable parameters in PReLU scale as x. So when we change the size of x, the whole network needs to be trained again, which is against our need for a network accepting arbitrary sized x.

--- toolbox/models.py
+++ toolbox/models.py
@@ -1,19 +1,18 @@
 from keras.layers import Conv2D
 from keras.layers import Conv2DTranspose
 from keras.layers import InputLayer
-from keras.layers import PReLU
 from keras.models import Sequential
 
 from toolbox.metrics import psnr
 
 
-def compile(model, optimizer='adam', loss='mse', metrics=(psnr,), **kwargs):
+def compile(model, optimizer='adam', loss='mse', metrics=[psnr], **kwargs):
     """Compile a model with default settings."""
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics, **kwargs)
     return model
 
 
-def build_srcnn(x, f=(9, 1, 5), n=(64, 32)):
+def build_srcnn(x, f=[9, 1, 5], n=[64, 32]):
     """Build an SRCNN model.
 
     See https://arxiv.org/abs/1501.00092
@@ -42,14 +41,13 @@ def build_fsrcnn(x, d=56, s=12, m=4, k=3):
     n = [d, s] + [s] * m + [d]
     for ni, fi in zip(n, f):
         model.add(Conv2D(ni, fi, padding='same',
-                         kernel_initializer='he_normal'))
-        model.add(PReLU())
+                         kernel_initializer='he_normal', activation='relu'))
     model.add(Conv2DTranspose(c, 9, strides=k, padding='same',
                               kernel_initializer='he_normal'))
     return model
 
 
-def build_espcn(x, f=(5, 3, 3), n=(64, 32), r=3):
+def build_espcn(x, f=[5, 3, 3], n=[64, 32], r=3):
     """Build an ESPCN model.
 
     See https://arxiv.org/abs/1609.05158
