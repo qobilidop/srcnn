cfdd9fab53de5507b11e1f2fca3283800c1841ee
Bili Dong <qobilidop@gmail.com>
2017-03-19 11:22:00 -0700

    Get input shape from data

--- toolbox/models.py
+++ toolbox/models.py
@@ -1,58 +1,66 @@
 from keras.layers import Conv2D
 from keras.layers import Conv2DTranspose
+from keras.layers import InputLayer
+from keras.layers import PReLU
 from keras.models import Sequential
 
 from toolbox.metrics import psnr
 
 
-def compile(model, optimizer='adam', loss='mse', metrics=[psnr], **kwargs):
+def compile(model, optimizer='adam', loss='mse', metrics=(psnr,), **kwargs):
     """Compile a model with default settings."""
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics, **kwargs)
     return model
 
 
-def srcnn(c=1, f1=9, f2=1, f3=5, n1=64, n2=32):
+def build_srcnn(x, f=(9, 1, 5), n=(64, 32)):
     """Build an SRCNN model.
 
     See https://arxiv.org/abs/1501.00092
     """
+    assert len(f) == len(n) + 1
     model = Sequential()
-    model.add(Conv2D(n1, f1, padding='same', kernel_initializer='he_normal',
-                     activation='relu', input_shape=(None, None, c)))
-    model.add(Conv2D(n2, f2, padding='same', kernel_initializer='he_normal',
-                     activation='relu'))
-    model.add(Conv2D(c, f3, padding='same', kernel_initializer='he_normal'))
+    model.add(InputLayer(input_shape=x.shape[-3:]))
+    c = x.shape[-1]
+    for ni, fi in zip(n, f):
+        model.add(Conv2D(ni, fi, padding='same',
+                         kernel_initializer='he_normal', activation='relu'))
+    model.add(Conv2D(c, f[-1], padding='same',
+                     kernel_initializer='he_normal'))
     return model
 
 
-def fsrcnn(c=1, d=56, s=12, m=4, k=3):
+def build_fsrcnn(x, d=56, s=12, m=4, k=3):
     """Build an FSRCNN model.
 
-    See http://link.springer.com/chapter/10.1007/978-3-319-46475-6_25
+    See https://arxiv.org/abs/1608.00367
     """
-    conv_params = dict(padding='same', kernel_initializer='he_normal',
-                       activation='relu')
     model = Sequential()
-    model.add(Conv2D(d, 5, input_shape=(None, None, c), **conv_params))
-    model.add(Conv2D(s, 1, **conv_params))
-    for i in range(m):
-        model.add(Conv2D(3, s, **conv_params))
-    model.add(Conv2D(d, 1, **conv_params))
-    model.add(Conv2DTranspose(1, 9, strides=k, padding='same',
+    model.add(InputLayer(input_shape=x.shape[-3:]))
+    c = x.shape[-1]
+    f = [5, 1] + [3] * m + [1]
+    n = [d, s] + [s] * m + [d]
+    for ni, fi in zip(n, f):
+        model.add(Conv2D(ni, fi, padding='same',
+                         kernel_initializer='he_normal'))
+        model.add(PReLU())
+    model.add(Conv2DTranspose(c, 9, strides=k, padding='same',
                               kernel_initializer='he_normal'))
     return model
 
 
-def espcn(c=1, f1=5, f2=3, f3=3, n1=64, n2=32, r=3):
+def build_espcn(x, f=(5, 3, 3), n=(64, 32), r=3):
     """Build an ESPCN model.
 
     See https://arxiv.org/abs/1609.05158
     """
+    assert len(f) == len(n) + 1
     model = Sequential()
-    model.add(Conv2D(n1, f1, padding='same', kernel_initializer='he_normal',
-                     activation='tanh', input_shape=(None, None, c)))
-    model.add(Conv2D(n2, f2, padding='same', kernel_initializer='he_normal',
-                     activation='tanh'))
-    model.add(Conv2D(c * r ** 2, f3, padding='same',
+    model.add(InputLayer(input_shape=x.shape[-3:]))
+    c = x.shape[-1]
+    for ni, fi in zip(n, f):
+        model.add(Conv2D(ni, fi, padding='same',
+                         kernel_initializer='he_normal', activation='tanh'))
+    model.add(Conv2D(c * r ** 2, f[-1], padding='same',
                      kernel_initializer='he_normal'))
     return model
