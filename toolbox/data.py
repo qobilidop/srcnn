8253f1def3540f133577eb57ce6e961c1bc17849
Bili Dong <qobilidop@gmail.com>
2017-03-19 23:10:55 -0700

    Refactor data loader
    Change arguments names and default values.

--- toolbox/data.py
+++ toolbox/data.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
@@ -7,21 +9,20 @@ from toolbox.image import modcrop
 from toolbox.paths import data_dir
 
 
-def load_set(name, sub_size=7, sub_stride=3, scale=3):
-    dataset_dir = data_dir / name
+def load_set(name, lr_sub_size=11, lr_sub_stride=5, scale=3):
+    hr_sub_size = lr_sub_size * scale
+    hr_sub_stride = lr_sub_stride * scale
+    lr_gen_sub = partial(generate_sub_images, size=lr_sub_size,
+                         stride=lr_sub_stride)
+    hr_gen_sub = partial(generate_sub_images, size=hr_sub_size,
+                         stride=hr_sub_stride)
+
     lr_sub_arrays = []
     hr_sub_arrays = []
-    for path in dataset_dir.glob('*'):
+    for path in (data_dir / name).glob('*'):
         lr_image, hr_image = load_image_pair(str(path), scale=scale)
-        gen_sub = generate_sub_images
-        lr_sub_arrays += [
-            img_to_array(img)
-            for img in gen_sub(lr_image, sub_size, sub_stride)
-        ]
-        hr_sub_arrays += [
-            img_to_array(img)
-            for img in gen_sub(hr_image, sub_size * scale, sub_stride * scale)
-        ]
+        lr_sub_arrays += [img_to_array(img) for img in lr_gen_sub(lr_image)]
+        hr_sub_arrays += [img_to_array(img) for img in hr_gen_sub(hr_image)]
     x = np.stack(lr_sub_arrays)
     y = np.stack(hr_sub_arrays)
     return x, y
@@ -35,7 +36,7 @@ def load_image_pair(path, scale=3):
     return lr_image, hr_image
 
 
-def generate_sub_images(image, crop_size, stride):
-    for i in range(0, image.size[0] - crop_size + 1, stride):
-        for j in range(0, image.size[1] - crop_size + 1, stride):
-            yield image.crop([i, j, i + crop_size, j + crop_size])
+def generate_sub_images(image, size, stride):
+    for i in range(0, image.size[0] - size + 1, stride):
+        for j in range(0, image.size[1] - size + 1, stride):
+            yield image.crop([i, j, i + size, j + size])
